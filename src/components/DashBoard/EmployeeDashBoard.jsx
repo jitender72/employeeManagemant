bd7d9db3ffae3aad4d4c9382263f12624e04f0ed
jitender <jitender0827@gmail.com>
2026-04-13 14:20:38 +0530

    make employee Dasboard to responsive

--- src/components/DashBoard/EmployeeDashBoard.jsx
+++ src/components/DashBoard/EmployeeDashBoard.jsx
@@ -4,7 +4,7 @@ import TaskListNumbers from "../otherComponents/TaskListNumbers";
 import TaskList from "../TaskList/TaskList";
 const EmployeeDashBoard = () => {
   return (
-    <div className="p-10">
+    <div className="p-10 bg-black">
       <h1 className="text-center text-white mb-6 text-5xl font-serif">
         Employee Dashboard
       </h1>
